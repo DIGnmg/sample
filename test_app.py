9deaf89d216e8651e6ff20dbbae35e4b851a27ac
Nathanael Gethers <gethernm@gmail.com>
2015-10-04 09:50:31 -0500

    fixing test

--- test_app.py
+++ test_app.py
@@ -6,12 +6,12 @@ import mainApp
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
-        mainApp.app.config['TESTING'] = True
-        self.mainApp = mainApp.app.test_client()
+        mainApp.mainApp.config['TESTING'] = True
+        self.mainApp = mainApp.mainApp.test_client()
 
     def test_index(self):
         index = self.mainApp.get('/')
-        assert 'Hello World' in index.data
+        assert 'mainApp' in index.data
 
 if __name__ == '__main__':
     unittest.main()
